0a1a56cc6162678be2d5655ab889866cb94f7d6b
Abdul Rehman <abdul.rehman@coeus-solutions.de>
2022-02-01 19:14:24 +0500

    Added div to hello world!

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
 
     <h1>Hello world!</h1>
+    <div>
+        <p>P tag in a div for new branch</p>
+    </div>
     <p>This is the first file in my new Git Repo.</p>
     <p>A new line in our file!</p>
 
